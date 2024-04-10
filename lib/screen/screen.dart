e34433111ffd7c805c93145f8e4b569a0d14db7d
Rojan <rojanparajuli13@gmail.com>
2024-04-10 22:22:58 +0545

    updates

--- lib/screen/screen.dart
+++ lib/screen/screen.dart
@@ -6,56 +6,59 @@ import 'package:hangman/widgets/guessword.dart';
 import 'package:hangman/widgets/remaining_attemts.dart';
 import 'package:hangman/widgets/resetbutton.dart';
 import 'package:confetti/confetti.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 
 class HangmanPage extends StatelessWidget {
   final HangmanController hangmanController = Get.put(HangmanController());
   final ConfettiController confettiController = ConfettiController();
-
+  final AudioPlayer clapPlayer = AudioPlayer();
+  final AudioPlayer booPlayer = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
     // ignore: deprecated_member_use
     return WillPopScope(
-        onWillPop: () async {
+      onWillPop: () async {
         bool exit = await showDialog(
           context: context,
           builder: (context) => AlertDialog(
             title: const Text(
               'Exit Game?',
               style: TextStyle(
-                color: Colors.red, 
-                fontWeight: FontWeight.bold, 
-                fontSize: 20.0, 
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
               ),
             ),
             content: const Text(
               'Are you sure you want to exit the Game?',
               style: TextStyle(
-                fontSize: 18.0, 
+                fontSize: 18.0,
               ),
             ),
-            backgroundColor: Colors.white, 
+            backgroundColor: Colors.white,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0), 
-              side: const BorderSide(color: Colors.blue, width: 2.0), 
+              borderRadius: BorderRadius.circular(10.0),
+              side: const BorderSide(color: Colors.blue, width: 2.0),
             ),
             actions: <Widget>[
               TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop(false); 
+                  Navigator.of(context).pop(false);
                 },
                 child: const Text('No'),
               ),
               TextButton(
                 onPressed: () {
-                  Navigator.of(context).pop(true); 
+                  Navigator.of(context).pop(true);
                 },
                 child: const Text('Yes'),
               ),
             ],
           ),
         );
-        return exit;
+        return exit; // Return false if exit is null
       },
       child: Scaffold(
         body: SafeArea(
@@ -65,7 +68,11 @@ class HangmanPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Obx(() => Text('Score: ${hangmanController.score.value}', style: const TextStyle(color: Colors.green, fontWeight: FontWeight.bold),)),
+                  Obx(() => Text(
+                        'Score: ${hangmanController.score.value}',
+                        style: const TextStyle(
+                            color: Colors.green, fontWeight: FontWeight.bold),
+                      )),
                   const SizedBox(height: 10,),
                   const RemainingAttemptsWidget(),
                   GuessedWordWidget(),
@@ -76,17 +83,20 @@ class HangmanPage extends StatelessWidget {
               ),
               Positioned.fill(
                 child: Obx(() {
-                  if (hangmanController.guessedWord.value == hangmanController.wordToGuess.value) {
+                  if (hangmanController.guessedWord.value ==
+                      hangmanController.wordToGuess.value) {
                     confettiController.play();
+                   clapPlayer.play('clapaudio.mp3' as Source);
                     return ConfettiWidget(
                       confettiController: confettiController,
                       blastDirectionality: BlastDirectionality.explosive,
                       shouldLoop: true,
                       colors: const [Colors.blue, Colors.red, Colors.green],
                     );
-                  } else {
-                    return const SizedBox.shrink();
+                  } else if (hangmanController.remainingAttempts.value == 0) {
+                    booPlayer.play('booaudio.mp3' as Source);
                   }
+                  return const SizedBox.shrink();
                 }),
               ),
             ],
